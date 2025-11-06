e1271d78ae9474283ee93d7054e960f18b09d03b
Ducratif <ducratiff@proton.me>
2025-11-06 22:09:48 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Ducratif'
+description 'Marché Dynamique - GuerreRP'
+version '1.0.0'
+
+shared_scripts {
+    '@ox_lib/init.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js',
+    'html/assets/*.png'
+}
